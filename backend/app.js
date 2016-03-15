fd9a3705434f4ab7cf9b037da712cf416a234d9d
Dan LaFeir <dlafeir@thoughtworks.com>
2016-03-15 18:54:31 -0500

    Dan [fix] Fix local setup to run the application; added some comments

--- backend/app.js
+++ backend/app.js
@@ -6,15 +6,20 @@ app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({ extended: true }) );
 
 //Set up to render the views correctly from the views folder
-app.engine('html', require('ejs').renderFile);
+app.set('html', __dirname.replace('backend', 'frontend') + '/html');
 app.set('views', __dirname.replace('backend', 'frontend') + '/html');
+
+//This allows for the js/ and css/ files to be served
 app.use(express.static(__dirname.replace('backend', 'frontend')));
+
+//This specifies what templating engine to use
+app.engine('html', require('ejs').renderFile);
 
 //Set up to use all the routes from router.js
 var router = require('./c35_modules/router');
 app.use('/', router);
 
 app.listen(port, function(){
-  console.log("Application is running:")
+  console.log("Application is up and running successfully");
   console.log("Listening on " + port);
 });
